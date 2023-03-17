e5b7c4bcdd866af9a28c28722b20c2c4599caffd
thiago.pescado@gmail.com <thiago.pescado@gmail.com>
2023-03-16 22:22:55 -0300

    add usequery hook and first react functiona component

--- src/web/ClientApp/src/components/Counter.js
+++ src/web/ClientApp/src/components/Counter.js
@@ -1,31 +1,6 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-export class Counter extends Component {
-  static displayName = Counter.name;
-
-  constructor(props) {
-    super(props);
-    this.state = { currentCount: 0 };
-    this.incrementCounter = this.incrementCounter.bind(this);
-  }
-
-  incrementCounter() {
-    this.setState({
-      currentCount: this.state.currentCount + 1
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Counter</h1>
-
-        <p>This is a simple example of a React component.</p>
-
-        <p aria-live="polite">Current count: <strong>{this.state.currentCount}</strong></p>
-
-        <button className="btn btn-primary" onClick={this.incrementCounter}>Increment</button>
-      </div>
-    );
-  }
+export function Counter(props) {
+    const [count, setCount] = useState(0);
+    return <h1>ula la la, {props.name}</h1>;
 }
